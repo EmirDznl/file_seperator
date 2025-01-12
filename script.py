60ed6d076402ce76eaa1695ae3d83a69921210dd
EmirDznl <duzenliemircan@gmail.com>
2025-01-12 11:50:40 +0000

    finished move_files function

--- script.py
+++ script.py
@@ -1,44 +1,51 @@
 import lib_platform
 import os
+import shutil
 
 def enter_downloads_path():
     pass
     #downloads_folder_path = input()
     #print(downloads_folder_path)
 
-download_dolder_path = "/home/emr/Downloads"
+download_folder_path = "/home/emr/Downloads"
 
 file_types = {
-    'Images': ['.jpg', '.jpeg', '.png'],
-    'Videos': ['.mp4', '.avi', '.mkv', '.mov'],
-    'Documents': ['.pdf', '.docx', '.txt', '.pptx'],
-    'Audio': ['.mp3', '.wav'],
-    'Archives': ['.zip', '.tar', '.rar', '.gz'],
-    'undefined' : []
-    }
+    "Images": ["jpg", "jpeg", "png"],
+    "gif": ["gif"],
+    "Videos": ["mp4", "avi", "mkv", "mov"],
+    "Documents": ["pdf", "docx", "txt", "pptx"],
+    "Audio": ["mp3", "wav"],
+    "Archives": ["zip", "tar", "rar", "gz"],    }
 
-def check_if_folder_exists(download_dolder_path):
+def check_if_folders_exists(download_folder_path):
     for file in file_types.keys():
-        path = os.path.join(download_dolder_path, file)
+        path = os.path.join(download_folder_path, file)
         if os.path.exists(path):
-            print(f"folder already exists: {file}")
+            pass
+            #print(f"folder already exists: {file}")
         else:
             try:
                 os.mkdir(path)
             except PermissionError:
                 print("need sudo permissions to create a folder")
 
-check_if_folder_exists(download_dolder_path)
+def move_files(download_folder_path):
+    i=0
+    for x in os.listdir(download_folder_path):
+        isfile_path = os.path.join(download_folder_path,x)
+        is_file = os.path.isdir(os.path.join(isfile_path))
+        
+        if not is_file:
+            ext = x.split(".")[-1]            
 
-def move_files(download_dolder_path):
-    for x in os.listdir(download_dolder_path):
-        isfile_path = os.path.join(download_dolder_path,x)
-        is_file = os.path.isfile(os.path.join(isfile_path))
-        print(x)
-        print(is_file)
-        if is_file:
-            ext = x.split(".")[-1]
-            print(ext)
-        print("----------------")
+            for folder , extension in file_types.items():
+                if ext in extension:
+                    ext_dest = folder
+                    final_dest = os.path.join(download_folder_path, folder, x)
+                    shutil.move(isfile_path, final_dest)
+                    i=i+1
+                    print(f"{isfile_path} moved into {folder} : {final_dest}, moved file number : {i}")
+                    print("-----------")
 
-move_files(download_dolder_path)
+check_if_folders_exists(download_folder_path)
+move_files(download_folder_path)
